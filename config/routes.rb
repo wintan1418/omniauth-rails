1b359c6ae35f31c7250e0bfd3bd77c0e7bdd088b
wintan1418 <wintan1418@gmail.com>
2022-10-27 10:05:56 +0100

    Finish up Api

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # resources :users
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
-  post "/login", to: "session#create"
-  delete "/logout", to: "session#destroy"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
