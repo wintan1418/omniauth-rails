dc29765043c5ada2f2c17ef29d91b99d136bdd4e
wintan1418 <wintan1418@gmail.com>
2022-10-27 09:23:33 +0100

    Set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
